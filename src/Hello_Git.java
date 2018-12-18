ec30cf6fe80dfcb4162ef74ceceb0814e853fe36
Lavanya <lavanya.m@8kmiles.com>
2018-12-18 12:43:03 +0530

    my commit

--- src/Hello_Git.java
+++ src/Hello_Git.java
@@ -2,12 +2,14 @@
 public class Hello_Git {
 
 	public static void main(String[] args) {
-		
-		
-	System.out.println("Hello Git added by me");
-		
-		
 
+		System.err.println("Hello Git added by me");
+
+		int a = 1, b = 2, c = 3;
+
+		a = a + b;
+		c = a + b;
+		System.out.println(c);
 	}
 
 }
